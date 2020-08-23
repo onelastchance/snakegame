ac84bca6a78d241ef7abd1506000dfc38f1c8680
onelastchance <65328902+onelastchance@users.noreply.github.com>
2020-08-23 21:40:05 +0530

    Code File

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame obj=new JFrame();
+		Gameplay gameplay = new Gameplay();
+        obj.setBounds(10, 10, 905, 700);
+        obj.setBackground(Color.DARK_GRAY);
+        obj.setVisible(true);
+        obj.setResizable(false);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+
+	}
+
+}
